7b8a406954e418f821292a69060102c649b8c654
guyam23-meet <guyamibar@gmail.com>
2022-08-05 11:30:47 +0300

    /

---
+++ app.py
@@ -0,0 +1,9 @@
+Config = {
+  "apiKey": "AIzaSyBSI-iNkNgm0yHq-EHFOq7LpKmL3rdOMrE",
+  "authDomain": "nefashot-proj.firebaseapp.com",
+  "projectId": "nefashot-proj",
+  "storageBucket": "nefashot-proj.appspot.com",
+  "messagingSenderId": "1083253811116",
+ "appId": "1:1083253811116:web:68d77ae866b32a2b767c09",
+  "measurementId": "G-8R4V5S49XD"
+}
