3d63d6a9f8a3605c36281043875c0f49b41f8046
riwaab23-meetmeow <109904805+riwaab23-meetmeow@users.noreply.github.com>
2022-08-08 17:52:55 +0300

    the newest template

--- app.py
+++ app.py
@@ -2,36 +2,59 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask import session as login_session
 import pyrebase
 
+
 config = {
-  "apiKey": "AIzaSyC2DHVKTK5dauvHhzqhsm8FVLrQqbtNc30",
-  "authDomain": "the-project-3a481.firebaseapp.com",
-  "projectId": "the-project-3a481",
-  "storageBucket": "the-project-3a481.appspot.com",
-  "messagingSenderId": "382041353587",
-  "appId": "1:382041353587:web:de4243a1bae8b43738e906",
-  "measurementId": "G-FPJ1G3R55F",
-  "databaseURL":"https://the-project-3a481-default-rtdb.europe-west1.firebasedatabase.app/"
+  "apiKey": "AIzaSyBSI-iNkNgm0yHq-EHFOq7LpKmL3rdOMrE",
+  "authDomain": "nefashot-proj.firebaseapp.com",
+  "projectId": "nefashot-proj",
+  "storageBucket": "nefashot-proj.appspot.com",
+  "messagingSenderId": "1083253811116",
+ "appId": "1:1083253811116:web:68d77ae866b32a2b767c09",
+  "measurementId": "G-8R4V5S49XD",
+  "databaseURL":"https://nefashot-proj-default-rtdb.europe-west1.firebasedatabase.app/"
 }
 
-firebase=pyrebase.initialize_app(config)
-auth=firebase.auth()
+
+firebase= pyrebase.initialize_app(config)
+auth = firebase.auth()
 db = firebase.database()
 
-@app.route('/volunteer', methods=['GET', 'POST'])
-def signup():
-   error = ""
-   if request.method == 'POST':
-       name = request.form['name']
-       number = request.form['number']
-       living = request.form['living']
-       reason = request.form['reason']
-       age = request.form['age']
-       mail = request.form['mail']
-       lang = request.form['lang']
-       try:
-       	login_session['user'] = 
-auth.create_user_with_name_and_number_and_living_and_reason_and_age_and_mail_and_lang(name,number,living,reason,age,mail,lang)
-           return redirect(url_for('home'))
-       except:
-           error = "Authentication failed"
-   return render_template("volunteer.html")
+app = Flask(__name__, template_folder='templates', static_folder='static')
+app.config['SECRET_KEY'] = 'super-secret-key'
+
+
+#start coding
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+  return render_template('about.html')
+
+@app.route('/blog', methods=['GET', 'POST'])
+def blog():
+  if request.method == 'POST':
+    if request.form['msg'] != "":
+      message = {"msg" : request.form['msg']}
+      db.child('Messages').push(message)
+      return render_template('blog.html', message = db.child('Messages').get().val(),admin=login_session['admin'])
+  return render_template('blog.html', message = db.child('Messages').get().val(),admin=login_session['admin'])
+
+@app.route('/admin',methods=['POST','GET'])
+def admin():
+  login_session['admin']=False
+  password="nefashot_admin123"
+  if request.method=='POST':
+    if request.form['password']==password:
+      login_session['admin']=True
+    else:
+      return render_template('admin.html',password=False,admin=login_session['admin'])
+  return render_template('admin.html',admin=login_session['admin'],password=True)
+
+def remove(i):
+  message = db.child('Messages').get().val()
+  message[i]['msg'].remove()
+
+#end coding
+
+
+if __name__ == '__main__':
+  app.run(debug=True)
