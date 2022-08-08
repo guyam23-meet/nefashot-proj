c46721ed09a9dbbcb09dd9e237fa77937d4cf663
guyam23-meet <guyamibar@gmail.com>
2022-08-08 16:00:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,14 +4,14 @@ import pyrebase
 
 
 config = {
-  "apiKey": "AIzaSyBSI-iNkNgm0yHq-EHFOq7LpKmL3rdOMrE",
-  "authDomain": "nefashot-proj.firebaseapp.com",
-  "projectId": "nefashot-proj",
-  "storageBucket": "nefashot-proj.appspot.com",
-  "messagingSenderId": "1083253811116",
+	"apiKey": "AIzaSyBSI-iNkNgm0yHq-EHFOq7LpKmL3rdOMrE",
+	"authDomain": "nefashot-proj.firebaseapp.com",
+	"projectId": "nefashot-proj",
+	"storageBucket": "nefashot-proj.appspot.com",
+	"messagingSenderId": "1083253811116",
  "appId": "1:1083253811116:web:68d77ae866b32a2b767c09",
-  "measurementId": "G-8R4V5S49XD",
-  "databaseURL":"https://nefashot-proj-default-rtdb.europe-west1.firebasedatabase.app/"
+	"measurementId": "G-8R4V5S49XD",
+	"databaseURL":"https://nefashot-proj-default-rtdb.europe-west1.firebasedatabase.app/"
 }
 
 
@@ -29,20 +29,20 @@ is_admin=False
 
 @app.route('/blog', methods=['GET', 'POST'])
 def blog():
-  if request.method == 'POST':
-    if request.form['msg'] != "":
-      message = {"msg" : request.form['msg']}
-      db.child('Messages').push(message)
-      return render_template('blog.html', message = db.child('Messages').get().val(),is_admin=is_admin)
-  return render_template('blog.html', message = db.child('Messages').get().val(),is_admin=is_admin)
+	if request.method == 'POST':
+		if request.form['msg'] != "":
+			message = {"msg" : request.form['msg']}
+			db.child('Messages').push(message)
+			return render_template('blog.html', message = db.child('Messages').get().val(),is_admin=is_admin)
+	return render_template('blog.html', message = db.child('Messages').get().val(),is_admin=is_admin)
 
 @app.route('/admin',mothods=['POST','GET'])
 def admin():
-  password="nefashot_admin123"
-  if request.method=='POST':
-    if request.form['password']==password:
-      is_admin=True
-  return render_template('admin.html',is_admin=is_admin)
+	password="nefashot_admin123"
+	if request.method=='POST':
+		if request.form['password']==password:
+			is_admin=True
+	return render_template('admin.html',is_admin=is_admin)
 
 
 
@@ -50,4 +50,4 @@ def admin():
 
 
 if __name__ == '__main__':
-  app.run(debug=True)
+	app.run(debug=True)
