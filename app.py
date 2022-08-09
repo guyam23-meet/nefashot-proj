c205ca2aa7f9b3b8a57ecc986a598fe1a0309803
guyam23-meet <guyamibar@gmail.com>
2022-08-09 10:30:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ app.config['SECRET_KEY'] = 'super-secret-key'
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-	if not login_session['admin']:
+	if 'admin' not login_session:
 		login_session['admin']=False
 	return render_template('about.html')
 
